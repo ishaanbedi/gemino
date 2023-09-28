2a1cec6a6e8fa78511a83f3f58014bc18e6ab57c
Ishaan Bedi <bediishaan6@gmail.com>
2023-09-28 10:46:14 +0530

    Remove unnecessary import in api/getQuestions.ts file

--- pages/api/getQuestions.ts
+++ pages/api/getQuestions.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getAuth } from "@clerk/nextjs/server";
-import { clerkClient } from "@clerk/nextjs";
 import axios from "axios";
 export default async function handler(
   req: NextApiRequest,
